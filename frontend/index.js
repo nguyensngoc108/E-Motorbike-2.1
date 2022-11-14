6d4edd5648ab34698aff7c0633d4d6dd1c8f0212
Canopus <nguyensngoc12@gmail.com>
2022-11-14 19:51:35 +0700

    Add html files

---
+++ frontend/index.js
@@ -0,0 +1,69 @@
+// const wrapper = document.querySelector(".aspect-ratio-169 img");
+// const menuItems1 = document.querySelectorAll(".menu");
+
+
+
+// menuItems1.forEach((item,index) => {
+//     item.addEventListener("click", () => {
+//         wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    
+//     })
+// })
+
+// var MenuItems = document.getElementById("MenuItems");
+// MenuItems.style.maxHeight = "0px";
+// function menutoggle(){
+//     if(MenuItems.style.maxHeight == "0px"){
+//         MenuItems.style.maxHeight = "200px";
+//     }
+//     else{
+//         MenuItems.style.maxHeight = "0px";
+//     }
+// } 
+
+const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
+const imgContainer = document.querySelector('.aspect-ratio-169')
+const dotItem = document.querySelectorAll(".dot")
+const header = document.querySelector(".header")
+let imgNum = imgPosition.length;
+let index = 0;
+
+     
+imgPosition.forEach(function(image,index){
+        image.style.left = index*100+"%"
+        dotItem[index].addEventListener("click" ,function(){
+                slider(index);
+                // imgContainer.style.left = "-" + index*100 +"%"
+        }
+        )
+})
+function imgSlide(){
+        index++;
+        if(index >= imgNum){
+                index = 0;
+        }
+        // function slider(index){
+                imgContainer.style.left = "-" + index*100 +"%"
+        // }
+}
+
+function slider(index){
+        imgContainer.style.left = "-" + index*100 +"%"
+        const dotAct = document.querySelector('.active');
+        dotAct.classList.remove("active")
+        dotItem[index].classList.add("active")
+
+}
+setInterval(imgSlide,5000);
+
+
+window.addEventListener("scroll",function(){
+        x = window.scrollY;
+        if(x>100){
+                header.classList.add("sticky");
+        }
+        else{
+                header.remove.add("sticky");
+        }
+
+})
