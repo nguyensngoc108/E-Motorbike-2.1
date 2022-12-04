3cb34459d88a872a8e49dc7166d79e01ba4ac3fd
Tran Thuan <dtthuan.1211@gmail.com>
2022-12-04 20:58:31 +0700

    The Final first time

--- theFinal/backend/admin/branddelete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "class/brand_class.php";
-$brand = new brand;
-$brand_id = $_GET['brand_id'];
-$delete_brand = $brand -> delete_brand($brand_id);
-
-?>
-
--- theFinal/backend/admin/productdelete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "class/product_class.php";
-$product = new product;
-$product_id = $_GET['product_id'];
-$delete_product = $product -> delete_product($product_id);
-
-?>
-
